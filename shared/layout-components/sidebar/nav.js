9029a568bbe9386bf7b6e33ebf1092680e1de2db
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2024-10-18 16:44:54 -0300

    fist capital letter dashboard

--- shared/layout-components/sidebar/nav.js
+++ shared/layout-components/sidebar/nav.js
@@ -3,7 +3,7 @@ export const MENUITEMS = [
         menutitle: 'GERAL',
     },
     {
-        title: 'dashboard',
+        title: 'Dashboard',
         icon: (
             <svg
                 xmlns="http://www.w3.org/2000/svg"
