9ea3a1254ccfdce4410b1ea064a325eecf458f05
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2024-10-22 14:12:21 -0300

    fixing problem on prisma

--- pages/api/server/db/prisma.js
+++ pages/api/server/db/prisma.js
@@ -1,15 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
-// declare global {
-//   var prisma: PrismaClient | undefined;
-// }
+const prisma = new PrismaClient({
+    log: ['query'],
+});
 
-export const prisma =
-    global.prisma ||
-    new PrismaClient({
-        log: ['query'],
-    });
-
-if (process.env.NODE_ENV !== 'production') {
-    global.prisma = prisma;
-}
+export default prisma;
