f14be41fffe9d1bf21bf9cd618b72adc5dc3110a
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2024-11-04 15:37:53 -0300

    title

--- shared/layout-components/seo/seo.js
+++ shared/layout-components/seo/seo.js
@@ -1,14 +1,14 @@
 import Head from 'next/head';
 import favicon from '../../../public/assets/images/brand-logos/favicon.ico';
 const Seo = ({ title }) => {
-    let i = `Solid Tecnologia - ${title}`;
+    let i = `Combo X3rec4 - ${title}`;
     return (
         <Head>
             <title>{i}</title>
             <link href={favicon.src} rel="icon"></link>
             <meta
                 name="description"
-                content="Solid Tecnologia - Nextjs Admin &amp; Dashboard Template"
+                content="Combo X3rec4 - Nextjs Admin &amp; Dashboard Template"
             />
             <meta
                 name="author"
